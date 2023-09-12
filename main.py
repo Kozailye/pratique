294a2816040b83504e8728d0e39eb72bac5b9750
Elie Kozaily <kozailye@educ.college-montreal.qc.ca>
2023-09-12 14:51:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# exercise 1
+#nom = input('Quel est ton nom?')
+#print('bonjour'+ nom)
+
+# exercise 2
+
+def somme(entier1, entier2):
+        adition = entier1 + entier2
+
+        #retern aditiion
+        return adition
+
+print(somme(5,3))
